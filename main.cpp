492c0ef02f9e929b3edc4c2a69822b6927f0841d
Cache Makers <Image15@iMac-H.local>
2017-02-28 17:45:17 -0700

    added a square that moves if keys are pressed

--- main.cpp
+++ main.cpp
@@ -8,17 +8,24 @@
 //Game Loop
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Wilson was Bored");
+  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Once there was a Square named Timothy");
 
-  sf::CircleShape circle(25);
+  sf::CircleShape circle(15);
   circle.setFillColor(sf::Color::Red);
   circle.setPosition(100,100);
+
+  sf::RectangleShape hero(sf::Vector2f(20,20));
+  hero.setFillColor(sf::Color::Green);
+  hero.setPosition(400,300);
+
   int circleDirection = 0;
+
+  double heroSpeed = 0.5;
 
 
   while(window.isOpen())
   {
-    //Handles Events
+    //Handle Only Certain Events
     sf::Event event;
     while(window.pollEvent(event))
     {
@@ -27,20 +34,41 @@ int main()
       {
         window.close();
       }
+
     }
+    //Handle Input NOW!!!
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
+    {
+      hero.move(0,-heroSpeed);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-hero.getSize().y)
+    {
+      hero.move(0,heroSpeed);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
+    {
+      hero.move(-heroSpeed,0);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-hero.getSize().x)
+    {
+      hero.move(heroSpeed,0);
+    }
+
+
     window.clear(sf::Color::Black);
 
-    //update
+    //UPDATE the WORLD!!!
+
     if(circleDirection == 0)
     {
-      circle.move(1,0);
+      circle.move(0.5,0);
     }
     if(circleDirection == 1)
     {
-      circle.move(-1,0);
+      circle.move(-0.5,0);
     }
 
-    if(circle.getPosition().x > 750)
+    if(circle.getPosition().x > 770)
     {
       circleDirection = 1;
     }
@@ -50,8 +78,10 @@ int main()
     }
 
 
-    //render fender bender sender
+
+    //RENDER ALL THE THINGS
     window.draw(circle);
+    window.draw(hero);
     window.display();
 
   }
